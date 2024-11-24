e932d6b7f21f52a00be1014fd917094b13e665c4
Alcides Ramos <info@alcidesrc.com>
2024-11-24 18:48:33 +0100

    feat: install terminal

--- install.sh
+++ install.sh
@@ -9,10 +9,10 @@ gsettings set org.gnome.desktop.screensaver lock-enabled false
 gsettings set org.gnome.desktop.session idle-delay 0
 
 
-echo "🔸 Installing fonts..."
+echo "\n\n🔸 Installing fonts..."
 source ~/.local/share/custom-setup/install/install-fonts.sh
 
-echo "🔸 Installing terminal..."
+echo "\n\n🔸 Installing terminal..."
 source ~/.local/share/custom-setup/install/install-terminal.sh
 
 
