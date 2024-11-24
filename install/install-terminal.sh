da80dcf97b9dcc42ba9bd82885e6c15a65d9308d
Alcides Ramos <info@alcidesrc.com>
2024-11-24 19:55:53 +0100

    feat: install applications

--- install/install-terminal.sh
+++ install/install-terminal.sh
@@ -5,7 +5,7 @@ sudo apt update -y && sudo apt upgrade -y && sudo apt install -y \
 curl -sS https://starship.rs/install.sh | sh -s -- --yes
 
 # Setup the new shell
-echo 'eval "$(starship init bash)"' >> ~/.bashrc
+grep -qxF 'eval "$(starship init bash)"' ~/.bashrc || echo 'eval "$(starship init bash)"' >> ~/.bashrc
 
 # Setup the Pastel-Powerline as preset
 starship preset pastel-powerline -o ~/.config/starship.toml
