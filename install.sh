c129910aa5b1b84244dabe68300cd10b39f9952f
Alcides Ramos <info@alcidesrc.com>
2024-11-24 18:31:40 +0100

    feat: install and check-version

--- install.sh
+++ install.sh
@@ -1,8 +1,21 @@
 # Exit immediately if a command exits with a non-zero status
 set -e
 
-# Desktop software and tweaks will only be installed if we're running Gnome
-RUNNING_GNOME=$([[ "$XDG_CURRENT_DESKTOP" == *"GNOME"* ]] && echo true || echo false)
-
 # Check the distribution name and version and abort if incompatible
 source ~/.local/share/custom-setup/install/check-version.sh
+
+# Ensure computer doesn't go to sleep or lock while installing
+gsettings set org.gnome.desktop.screensaver lock-enabled false
+gsettings set org.gnome.desktop.session idle-delay 0
+
+
+echo "🔸 Installing fonts..."
+source ~/.local/share/custom-setup/install/install-fonts.sh
+
+echo "🔸 Installing terminal..."
+source ~/.local/share/custom-setup/install/install-terminal.sh
+
+
+# Revert to normal idle and lock settings
+gsettings set org.gnome.desktop.screensaver lock-enabled true
+gsettings set org.gnome.desktop.session idle-delay 300
