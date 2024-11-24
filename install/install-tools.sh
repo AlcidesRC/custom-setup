4847494f89ded656bff2ce8c2e8247daa0e49bc7
Alcides Ramos <info@alcidesrc.com>
2024-11-24 19:52:15 +0100

    feat: install applications

--- install/install-tools.sh
+++ install/install-tools.sh
@@ -1,4 +1,8 @@
-sudo apt update -y && sudo apt upgrade -y && sudo apt install -y \
-  eza \       # <ls> replacement (see aliases for further details)
-  fd-find \   # <fd> replacement (see aliases for further details)
-  fzf         # <ff> replacement (see aliases for further details)
+# <ls> replacement (see aliases for further details)
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y eza
+
+# <fd> replacement (see aliases for further details)
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y fd-find
+
+# <ff> replacement (see aliases for further details)
+sudo apt update -y && sudo apt upgrade -y && sudo apt install -y fzf
