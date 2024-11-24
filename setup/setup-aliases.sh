1b0315f0f3c0e953c1daa35240d69710414edc16
Alcides Ramos <info@alcidesrc.com>
2024-11-24 20:07:31 +0100

    feat: install applications

--- setup/setup-aliases.sh
+++ setup/setup-aliases.sh
@@ -1,5 +1,3 @@
 [ -e ~/.bash_aliases ] && unlink ~/.bash_aliases
-
-ln -s ~/.local/share/custom-setup/bash/aliases ~/.bash_aliases
-
+ln -s ~/.local/share/custom-setup/defaults/bash/aliases ~/.bash_aliases
 source ~/.bashrc
