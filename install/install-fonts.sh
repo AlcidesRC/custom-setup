85f6a7b4057a4fd6c9c8bcb0136e6564d3b3e135
Alcides Ramos <info@alcidesrc.com>
2024-11-24 20:44:32 +0100

    feat: install applications

--- install/install-fonts.sh
+++ install/install-fonts.sh
@@ -9,10 +9,5 @@ unzip FiraCode.zip -d FiraCode
 cp FiraCode/*.ttf ~/.local/share/fonts
 rm -rf FiraCode.zip FiraCode
 
-wget https://github.com/ryanoasis/nerd-fonts/releases/download/v3.3.0/CascadiaMono.zip
-unzip CascadiaMono.zip -d CascadiaMono
-cp CascadiaMono/*.ttf ~/.local/share/fonts
-rm -rf CascadiaMono.zip CascadiaMono
-
 fc-cache
 cd -
