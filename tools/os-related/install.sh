04818149d7d88d725a6fa66480be6231eddb13f5
Alcides Ramos <info@alcidesrc.com>
2024-12-01 19:22:49 +0100

    feat: refactor

--- tools/os-related/install.sh
+++ tools/os-related/install.sh
@@ -1,6 +1,4 @@
-sudo apt update -y && sudo apt install -y \
-    curl \
-    eza \       # <ls> replacement (see aliases for further details)
-    fd-find \   # <fd> replacement (see aliases for further details)
-    fzf \       # <ff> replacement (see aliases for further details)
-    unzip
+# <eza> is a <ls> replacement (see aliases for further details)
+# <fd-find> is a <fd> replacement (see aliases for further details)
+# <fzf> is a <ff> replacement (see aliases for further details)
+sudo apt update -y && sudo apt install -y curl eza fd-find fzf unzip
