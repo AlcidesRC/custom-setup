80d9a6849cdddeb8652995b070c397ee56e158ba
Alcides Ramos <info@alcidesrc.com>
2024-12-07 18:30:15 +0100

    WIP

--- setup/themes/setup-light-theme.sh
+++ setup/themes/setup-light-theme.sh
@@ -3,7 +3,7 @@ gsettings set org.gnome.shell.ubuntu color-scheme prefer-light
 gsettings set org.gnome.desktop.interface color-scheme prefer-light
 
 # Set theme
-gsettings set org.gnome.desktop.interface gtk-theme Yaru-sage
+gsettings set org.gnome.desktop.interface gtk-theme Yaru-red
 
 # Set wallpaper
 gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/.local/share/custom-setup/defaults/wallpapers/light.jpg
