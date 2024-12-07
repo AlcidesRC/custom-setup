78eabb9807aadce26d4ff220b8fe80b6f191f27a
Alcides Ramos <info@alcidesrc.com>
2024-12-07 18:11:38 +0100

    WIP

--- setup/themes/setup-dark-theme.sh
+++ setup/themes/setup-dark-theme.sh
@@ -6,4 +6,4 @@ gsettings set org.gnome.desktop.interface color-scheme prefer-dark
 gsettings set org.gnome.desktop.interface gtk-theme Yaru-purple-dark
 
 # Set wallpaper
-gsettings set org.gnome.desktop.background picture-uri-dark file:///home/$USER/.local/share/custom-setup/defaults/wallpapers/dark.jpg
+gsettings set org.gnome.desktop.background picture-uri-dark file:///home/$USER/.local/share/custom-setup/defaults/wallpapers/dark.png
--- setup/themes/setup-light-theme.sh
+++ setup/themes/setup-light-theme.sh
@@ -6,4 +6,4 @@ gsettings set org.gnome.desktop.interface color-scheme prefer-light
 gsettings set org.gnome.desktop.interface gtk-theme Yaru-sage
 
 # Set wallpaper
-gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/.local/share/custom-setup/defaults/wallpapers/light.jpg
+gsettings set org.gnome.desktop.background picture-uri file:///home/$USER/.local/share/custom-setup/defaults/wallpapers/light.png
