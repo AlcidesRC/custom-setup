9fd24d27458ead504da59bbe9c078343ce83434a
Alcides Ramos <info@alcidesrc.com>
2024-12-13 14:52:57 +0100

    fix(gum): improved UI

--- tools/os-related/install.sh
+++ tools/os-related/install.sh
@@ -2,24 +2,19 @@ echo ':small_orange_diamond: Installing {{ Bold "OS-related tools" }}' | gum for
 echo ''
 
 # <eza> is a <ls> replacement (see aliases for further details)
-sudo apt-get install -y eza
-
 # <fd-find> is a <fd> replacement (see aliases for further details)
-sudo apt-get install -y fd-find
-
 # <fzf> is a <ff> replacement (see aliases for further details)
-sudo apt-get install -y fzf
-
 # <tilix> is a <terminal> replacement
-sudo apt-get install -y tilix
+sudo apt-get install -y
+  eza \
+  fd-find \
+  fzf \
+  gnome-tweaks \
+  gnome-shell-extensions \
+  make \
+  tilix \
+  unzip
+
 source ~/.local/share/custom-setup/defaults/app-tilix.sh
 
-# Install Gnome shell extensions
-sudo apt-get install -y \
-  gnome-tweaks \
-  gnome-shell-extensions
-
-# Install additional tools
-sudo apt-get install -y \
-  make \
-  unzip
+echo ''
